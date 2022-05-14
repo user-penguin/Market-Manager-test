73ef27761848595f699b0a7b4767e754bf40b522
user-penguin <kobzev96@gmail.com>
2022-05-14 17:37:11 +0700

    Рефакторинг с целью понизить цикломатическую сложность

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-/*Package solution
+/*Package sulution
 Задана строка S из малых латинских букв,
 требуется узнать длину наибольшей подстроки
 состоящей не более чем из двух различных символов.
@@ -6,41 +6,46 @@
 package sulution
 
 func Run(src string) int {
-	var max int
-	for i := range src {
-		if length := LargestLen(src[i:]); length > max {
-			max = length
-		}
-		// если максимальная длина подстроки больше, чем длина хвоста, останавливаемся
-		if max >= len(src[i:]) {
-			break
-		}
-	}
-	return max
-}
-
-func LargestLen(src string) int {
-	// подстроки типа "", "a", "ad", длины меньше 3, не нуждаются в дополнительном поиске
+	// строки с 0,1,2 символами пройдут всегда
 	if len(src) < 3 {
 		return len(src)
 	}
-	// массив рун для хранения искомой подстроки
-	var sub []rune
-	// мапа для хранения использованных символов
-	abc := make(map[rune]interface{})
-	// проходим по подстроке с начала
-	for _, k := range src {
-		// если символ уже в алфавите, просто присоединяем его к искомой подстроке
-		if _, ok := abc[k]; ok {
-			sub = append(sub, k)
-			// если символ новый, то смотрим, какой он по счёту, если не 3+, то запоминаем его и присоединяем к подстроке
-		} else if !ok && len(abc) < 2 {
-			abc[k] = struct{}{}
-			sub = append(sub, k)
-			// в противном случае возвращаем длину найденной подстроки
-		} else {
-			return len(sub)
+
+	var max, left, right int
+	runeSrc := []rune(src)    // приводим для удобства
+	var symbols []int         // слайс для индексов первых включений символов
+	abc := make(map[rune]int) // мапа для проверки уникальности
+
+	for i := 0; i < len(src); i++ {
+		if _, ok := abc[runeSrc[i]]; ok { // уже имеющийся символ
+			right = i
+		} else if !ok && len(abc) < 2 { // новый символ
+			right = i
+			abc[runeSrc[i]] = i
+			symbols = append(symbols, i)
+		} else { // новый символ, но выходящий за словие не более 2
+			length := right - left + 1
+			if length > max {
+				max = length
+			}
+			// надо перевести каретку на более позднее включение одного из символов
+			to := symbols[0]
+			if symbols[1] > to {
+				to = symbols[1]
+			}
+			// приводим параметры к новому началу анализа
+			i, left, right = to, to, to
+			// обнуляем мапу и слайс
+			abc = make(map[rune]int)
+			abc[runeSrc[i]] = i
+			// готовим контейнеры к новой итерации
+			symbols = make([]int, 0)
+			symbols = append(symbols, i)
 		}
 	}
-	return len(sub)
+	// если прошли всю строку, а она состояла не более чем из двух символов
+	if max == 0 {
+		return right - left + 1
+	}
+	return max
 }
